d8a88368d8e4dd5d24d8204b7a75f01c8214ce8e
ivamtra <ivanmar009@gmail.com>
2022-04-04 19:27:57 +0000

    ruletta getur nu snuid

--- src/main/java/hi/verkefni/vidmot/RouletteController.java
+++ src/main/java/hi/verkefni/vidmot/RouletteController.java
@@ -1,9 +1,23 @@
 package hi.verkefni.vidmot;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
-public class RouletteController {
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class RouletteController implements Initializable {
+
+    //TODO hægt að setja getNumber í sér klasa
 
     @FXML
     private Button fxSpinButton;
@@ -11,17 +25,82 @@ public class RouletteController {
     @FXML
     private ImageView fxRouletteImage;
 
+    private Timeline timeline;
+
+    @FXML
+    private Pane fxPane;
+
+
+    // Offset = -2.6 gradur
+    // Þ.e. 00 er i -2.6 gradum
+
+    // -1 taknar 00
+    private int[] numbers = {-1, 27, 10, 35, 29, 12, 8, 19, 31, 18, 6, 21, 33, 16, 4, 23, 35, 14, 2, 0, 28, 9, 26, 30, 11, 7, 20, 32, 17, 5, 22, 34, 15, 3, 24, 36, 13, 1};
+
 
     public void spin() throws InterruptedException {
 
+        // Horn eru í gráðum
+        double doubleInitialVelocity = Math.round((Math.random()*10.0 + 5)*100.0)/100.0;
 
-        double initialVelocity = Math.random()*10.0;
+        //Math.round(x*100.0)/100.0 gefur 2 aukastafi
 
-        while (initialVelocity > 0.0) {
-            System.out.println(initialVelocity);
-            fxRouletteImage.setRotate(fxRouletteImage.getRotate() + initialVelocity);
-            initialVelocity -= 0.002;
-            Thread.sleep(20);
+        AtomicReference<Double> initialVelocity = new AtomicReference<>(doubleInitialVelocity); // gradur/sek
+        KeyFrame keyFrame = new KeyFrame(Duration.millis(10),
+                e -> {
+                    fxRouletteImage.setRotate(fxRouletteImage.getRotate() + initialVelocity.get());
+                    initialVelocity.updateAndGet(v -> new Double((double) (v - 0.01)));
+                });
+
+        System.out.println(initialVelocity);
+
+        timeline = new Timeline(keyFrame);
+        timeline.setCycleCount((int) doubleInitialVelocity*100);
+        timeline.play();
+
+        timeline.setOnFinished(e ->
+                {
+                    System.out.println(getnumber());
+                }
+                );
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        fxRouletteImage.setRotate(Math.random()*360);
+        System.out.println(fxRouletteImage.getRotate());
+
+        System.out.println(getnumber());
+
+        Polygon triangle = new Polygon();
+
+        double initialX = 175.0;
+        double initialY = 24.0;
+        double length = 10;
+        triangle.getPoints().setAll(
+                initialX-length, initialY,
+                initialX+length, initialY,
+                initialX, initialY+length
+        );
+        triangle.setFill(Color.BLUE);
+        fxPane.getChildren().add(triangle);
+
+    }
+
+    public int getnumber() {
+        double angle = fxRouletteImage.getRotate() % 360;
+
+        double angleOfNumber = 360 - angle;
+
+        double offSet = -2.6;
+        double step = 360/38.0; // Gradur sem hver tala tekur
+
+        int iterator = 0;
+
+        while(iterator*step - offSet < angleOfNumber) {
+            iterator++;
         }
+        return numbers[iterator];
     }
 }
